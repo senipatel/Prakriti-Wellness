f6bb66d62fc8730b1ce593877f9137dcc190d986
senipatel <senipatel017@gmail.com>
2025-09-07 23:13:24 +0530

    update Speed Insights

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,6 +13,7 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
       <Header />
       <main className="flex-1">
         {children}
+        <SpeedInsights />
       </main>
       <Footer />
     </div>
